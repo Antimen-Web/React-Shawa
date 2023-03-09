fc20549e052feaf6158b4521cf7c946a3f571567
Ilya <80890602+Antimen-Web@users.noreply.github.com>
2023-03-09 22:46:53 +0900

    Add files via upload

--- src/functions/payment.js
+++ src/functions/payment.js
@@ -25,9 +25,8 @@ exports.handler = async function (event, context) {
           token: token,
         },
       },
-      confirm: {
-        return_url: "https://bespoke-scone-ed21f0.netlify.app/",
-      },
+      confirm: true,
+      return_url: "https://bespoke-scone-ed21f0.netlify.app/",
       metadata: {
         items: JSON.stringify(items),
       },
