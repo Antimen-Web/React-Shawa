c3136026bd5feb46668fce7033cd4e7018725742
Ilya <80890602+Antimen-Web@users.noreply.github.com>
2023-03-09 16:31:07 +0900

    Add files via upload

--- src/functions/payment.js
+++ src/functions/payment.js
@@ -1,18 +1,29 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
+console.log(process.env.STRIPE_SECRET_KEY);
+
 exports.handler = async function (event, context) {
   try {
     const data = JSON.parse(event.body);
-    const { token, items, total } = data;
+    const { token, items, totalPrice } = data;
+
+    console.log(
+      "Creating payment intent with items:",
+      items,
+      "and total:",
+      total
+    );
 
     const paymentIntent = await stripe.paymentIntents.create({
       items: items,
-      amount: total,
+      amount: totalPrice,
       currency: "usd",
       description: "Payment for items",
       payment_method: token,
       confirm: true,
     });
+
+    console.log("Payment intent created:", paymentIntent);
 
     // сохранение информации об оплате в базу данных
 
@@ -21,6 +32,8 @@ exports.handler = async function (event, context) {
       body: JSON.stringify({ success: true }),
     };
   } catch (error) {
+    console.log("Error creating payment intent:", error);
+
     return {
       statusCode: 400,
       body: JSON.stringify({ error: error.message }),
